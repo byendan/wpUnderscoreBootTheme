9a78f2c0a77d37c91ba90842d9d43109d2becee7
byendan <byendanb@gmail.com>
2015-12-02 13:04:35 -0800

    Working post template

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,13 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php peppers_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+	
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -28,7 +22,5 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php peppers_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+
 </article><!-- #post-## -->
